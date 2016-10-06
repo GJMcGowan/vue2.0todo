b52160ec6c9b95ecc71f590ada2d97953e2c4ab6
George McGowan <geomcgowan@deloitte.co.uk>
2016-10-06 14:32:19 +0100

    Initial Commit

---
+++ src/components/List.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class='list'>
+    <ul>
+      <li v-for='todo in todos'>
+        <span class='col-xs-10 todo-title'>{{ todo.title }}</span>
+        <button class='col-xs-2 btn btn-danger' v-on:click='removeItem($index)'>X</button>
+      </li>
+    </ul>
+    <input label='Item' v-model='newTodo'>
+    <button class='btn btn-default' v-on:click='addItem'>Add Item</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        todos: [],
+        newTodo: ''
+      }
+    },
+    methods: {
+      addItem () {
+        const todo = this.newTodo.trim()
+        if (todo) {
+          this.todos.push({ title: todo })
+          this.newTodo = ''
+        }
+      },
+      removeItem (index) {
+        this.todos.splice(index, 1)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  li {
+    list-style: none
+  }
+
+  .todo-title {
+    font-size: 3rem;
+  }
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="todo">
+    <h1>{{ title }}</h1>
+    <h3>{{ subtitle }}</h3>
+    <list />
+  </div>
+</template>
+
+<script>
+  import List from './List.vue'
+
+  export default {
+    data () {
+      return {
+        title: 'Vue Todo App!',
+        subtitle: 'Add some items below!'
+      }
+    },
+    components: {List}
+  }
+</script>
+
+<style scoped>
+  h1, h2 {
+    color: #42b983;
+  }
+</style>
